0d1a97dd1e1a157b966a7f1d0604f7e48a1be483
Ajay Vangara <vangaraajay@gmail.com>
2025-12-27 19:47:01 -0500

    Added buttons to explore

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
       <p>Hi! I’m Ajay Vangara, a student developer at the University of Florida. I've built a variety of projects including full-stack web apps, mobile apps, and cloud-based systems, experimenting with AI and infrastructure along the way. 
         I love creating technology that can have a strong impact on society, both with the use of novel AI and without!</p>
       <p>Feel free to explore my skills and projects below, and don't hesitate to reach out if you'd like to connect!</p>
-
+      <a href="#skills">Skills</a>
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
 
       <section id="skills">
         <h1>Skills</h1>
