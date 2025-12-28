21e1102cff25b38d5321e594ba7217ec994cf06c
Ajay Vangara <vangaraajay@gmail.com>
2025-12-28 13:16:46 -0500

    Configured Tailwind

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <>     
-        <h1>Who am I?</h1>
+        <h1 className = "text-blue-500">Who am I?</h1>
         <p>Hi! I’m Ajay Vangara, a student developer at the University of Florida. I've built a variety of projects including full-stack web apps, mobile apps, and cloud-based systems, experimenting with AI and infrastructure along the way. 
             I love creating technology that can have a strong impact on society, both with the use of novel AI and without!</p>
         <p>Feel free to explore my skills and projects below, and don't hesitate to reach out if you'd like to connect!</p>
